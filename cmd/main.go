51e11afd65fb73fd5b4d2bfbf5e7481d68b2a09d
bryanjue <bryanandresjuelariofrio@gmail.com>
2025-03-22 22:04:57 +0100

    added the api rest to the main and one rout of testing

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	var greeting string
-	greeting = "Hello my friend "
-	fmt.Println(greeting)
+	app := gin.Default()
+
+	// Ruta GET
+	app.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "¡Hola, mundo!"})
+	})
+
+	// Iniciar servidor en el puerto 8080
+	app.Run(":8080")
 }
