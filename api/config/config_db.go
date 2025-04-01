6c95fc6f8ead0bc25b117c29e6ed7fc3faeea47e
bryanjue <bryanandresjuelariofrio@gmail.com>
2025-04-01 18:58:50 +0200

    added the configuration of the conexion to the database

---
+++ api/config/config_db.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"localhost",
+		"user_tpv",
+		"12345678",
+		"tpv",
+		"5432",
+	)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+
+	fmt.Println("Database connection successfully established")
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"VenusX/api"
+	"VenusX/api/config"
 )
 
 func main() {
+	// Dbconfiguration
+	// connect to the database
+	config.ConnectDB()
+	db := config.GetDB()
 	// start the api rest from package api
 	app := api.NewApp()
 	// run the app
