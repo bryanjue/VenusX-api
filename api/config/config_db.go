03b461fa53563e17775bd20455db1012d722b678
bryanjue <bryanandresjuelariofrio@gmail.com>
2025-04-20 20:30:26 +0200

    added an .env to get more security in the access into data base

--- api/config/config_db.go
+++ api/config/config_db.go
@@ -4,7 +4,9 @@ import (
 	"VenusX/api/models"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,16 +14,21 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error cargando el archivo .env")
+	}
+
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		"localhost",
-		"user_tpv",
-		"12345678",
-		"tpv",
-		"5432",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
 	)
 
-	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database")
