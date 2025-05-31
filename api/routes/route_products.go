a01a8b7a493239d930522ebfc3e89a96f4d371d9
bryanjue <bryanandresjuelariofrio@gmail.com>
2025-05-31 12:02:15 +0200

    I reunify the routes of products in only one file

--- api/routes/route_products.go
+++ api/routes/route_products.go
@@ -13,3 +13,14 @@ func SetupSearchRoute(router *gin.Engine) {
 func SetupDeleteProductRoute(router *gin.Engine) {
 	router.DELETE("/delete_product/:id", controllers.DeleteProduct)
 }
+
+// SetupAddProduct sets up the route for adding a product
+
+func SetupAddProduct(router *gin.Engine) {
+	router.POST("/add_product", controllers.CreateProducts)
+}
+
+// SetupSearchRoute configura la ruta de búsqueda
+func SetupGetAllProductsRoute(router *gin.Engine) {
+	router.GET("/getproducts", controllers.GetProducts)
+}
